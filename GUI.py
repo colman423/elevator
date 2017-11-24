f81997add687a4d4072755411656ae3b861eaf88
Bin-Jing <jeff20001234@yahoo.com.tw>
2017-11-24 10:51:25 +0800

    person can stop

--- GUI.py
+++ GUI.py
@@ -5,8 +5,7 @@ pygame.init()
 
 SIZE = WIDTH, HEIGHT = 720, 480
 BACKGROUND_COLOR = pygame.Color('white')
-FPS = 5
-
+FPS = 30
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 playerX = []
@@ -36,9 +35,9 @@ def quit_program():
 def create_person():  # create a person ui
     
     images = load_images()  # Make sure to provide the relative or full path to the images directory.
-    player = AnimatedSprite(position=(100, 100+len(playerX)*100), images=images)
+    player = AnimatedSprite(position=(100, 100+(len(playerX))%4*100), images=images)
     playerX.append(player)
-    
+    player.velocity.x = 1
     return 0
 
 
@@ -53,7 +52,7 @@ def person_leaving(person):  # let arrived person leave the window
 
 
 class AnimatedSprite(pygame.sprite.Sprite):
-
+    
     def __init__(self, position, images):
         """
         Animated sprite object.
@@ -65,7 +64,8 @@ class AnimatedSprite(pygame.sprite.Sprite):
         super(AnimatedSprite, self).__init__()
 
         size = (32, 32)  # This should match the size of the images.
-
+        posirionX = position[0]
+        positionY = position[1]
         self.rect = pygame.Rect(position, size)
         self.images = images
         self.images_right = images
@@ -75,15 +75,18 @@ class AnimatedSprite(pygame.sprite.Sprite):
 
         self.velocity = pygame.math.Vector2(0, 0)
 
-        self.animation_time = 0.1
+        self.animation_time = 0.3
         self.current_time = 0
 
         self.animation_frames = 6
         self.current_frame = 0
 
     def update_time_dependent(self, dt):
+        space_ship = pygame.image.load("elevator.png").convert_alpha()
+
+        space_ship_rect = space_ship.get_rect()
         """
-        Updates the image of Sprite approximately every 0.1 second.
+        Updates the image of Sprite approximately every 0.3 second.
 
         Args:
             dt: Time elapsed between each frame.
@@ -98,25 +101,26 @@ class AnimatedSprite(pygame.sprite.Sprite):
             self.current_time = 0
             self.index = (self.index + 1) % len(self.images)
             self.image = self.images[self.index]
-
+#        if self.rect.center[0] > 200:
+#            self.velocity.x = 0
         self.rect.move_ip(*self.velocity)
 
-    def update_frame_dependent(self):
-        """
-        Updates the image of Sprite every 6 frame (approximately every 0.1 second if frame rate is 60).
-        """
-        if self.velocity.x > 0:  # Use the right images if sprite is moving right.
-            self.images = self.images_right
-        elif self.velocity.x < 0:
-            self.images = self.images_left
-
-        self.current_frame += 1
-        if self.current_frame >= self.animation_frames:
-            self.current_frame = 0
-            self.index = (self.index + 1) % len(self.images)
-            self.image = self.images[self.index]
-
-        self.rect.move_ip(*self.velocity)
+#    def update_frame_dependent(self):
+#        """
+#        Updates the image of Sprite every 6 frame (approximately every 0.1 second if frame rate is 60).
+#        """
+#        if self.velocity.x > 0:  # Use the right images if sprite is moving right.
+#            self.images = self.images_right
+#        elif self.velocity.x < 0:
+#            self.images = self.images_left
+#
+#        self.current_frame += 1
+#        if self.current_frame >= self.animation_frames:
+#            self.current_frame = 0
+#            self.index = (self.index + 1) % len(self.images)
+#            self.image = self.images[self.index]
+#
+#        self.rect.move_ip(*self.velocity)
 
     def update(self, dt):
         """This is the method that's being called when 'all_sprites.update(dt)' is called."""
@@ -127,8 +131,9 @@ class AnimatedSprite(pygame.sprite.Sprite):
 
 def main():
     create_person()
-    all_sprites = pygame.sprite.Group(playerX)  # Creates a sprite group and adds 'player' to it.
-    
+#    timer = 0.0
+    count = 0
+    stopDistance = 200
     running = True
     while running:
         
@@ -141,37 +146,25 @@ def main():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_a:
                         create_person()
-                        all_sprites = pygame.sprite.Group(playerX)
-                for i in range(0,len(playerX)):
-                    playerX[i].velocity.x = 5
-    
+        if count < len(playerX):
+            if playerX[count].rect.center[0] > stopDistance:
+                playerX[count].velocity.x = 0
+                count += 1
+            
+                            
+#            timer += dt*5
+#            if timer >= 3:
+#                timer = 0
+#                playerX[count].velocity.x = 0
+#                if count <= len(playerX):
+#                    count += 1
+        all_sprites = pygame.sprite.Group(playerX) # Creates a sprite group and adds 'player' to it.
         all_sprites.update(dt)  # Calls the 'update' method on all sprites in the list (currently just the player).
         
         screen.fill(BACKGROUND_COLOR)
         all_sprites.draw(screen)
         pygame.display.update()
-#time.sleep(5)
-#create_person()
-#    images = load_images()  # Make sure to provide the relative or full path to the images directory.
-#    player = AnimatedSprite(position=(100, 100), images=images)
-#    all_sprites = pygame.sprite.Group(player)  # Creates a sprite group and adds 'player' to it.
-#
-#    running = True
-#    while running:
-#
-#        dt = clock.tick(FPS) / 1000  # Amount of seconds between each loop.
-#
-#        for event in pygame.event.get():
-#            if event.type == pygame.QUIT:
-#                running = False
-#            else:
-#                player.velocity.x = 4
-#
-#        all_sprites.update(dt)  # Calls the 'update' method on all sprites in the list (currently just the player).
-#
-#        screen.fill(BACKGROUND_COLOR)
-#        all_sprites.draw(screen)
-#        pygame.display.update()
+
 
 
 if __name__ == '__main__':
