3ed7cbebc10c0a6b29c0ab350e1b7daf460969ff
WilsonWang <wilson8507@gmail.com>
2017-12-10 17:57:51 +0800

    Add multiple people entering elevator
    * Add multiple people entering elevator when exiting
    * Add multiple people entering elevator when entering

--- elevator.py
+++ elevator.py
@@ -5,34 +5,50 @@ from collections import defaultdict, OrderedDict
 class Elevator(threading.Thread):
     def __init__(self):
         super().__init__(name="elevator")
-        self.toPass = []
+        self.toTransport = []
         self.waiting_list = defaultdict(list)
         self.waiting_num = 0
-        # self.lock = threading.Lock()
         self.passenger_num = 0
         self.active = False
         self.fromFloor = 0
         self.toFloor = 0
 
     def take_passenger(self, atFloor):
+        time.sleep(0.1)
         self.toFloor = atFloor
-        self.passenger_num += 1
+        self.passenger_num += len(self.waiting_list[atFloor])
+        self.waiting_num -= len(self.waiting_list[atFloor])
         print("Elevator move from {} to {} ({} peoples in elevator)".format(self.fromFloor, self.toFloor, self.passenger_num))
-
-    def transport(self, toFloor):
+        print("{} people enter elevator".format(len(self.waiting_list[atFloor])))   
+        self.toTransport += self.waiting_list[atFloor]
+        self.waiting_list[atFloor] = []
+        print('toTransport: ', self.toTransport)
+        
+    def transport(self):
+        time.sleep(0.1)
         self.fromFloor = self.toFloor
-        self.toFloor = toFloor
-        self.passenger_num -= 1
+        self.toFloor = self.toTransport.pop() #  need to implement a exiting prioirtiy to choose which floor the elevator should go to
+        self.passenger_num -= 1 # reduce the same desitnation people num
         print("Elevator move from {} to {} ({} peoples in elevator)".format(self.fromFloor, self.toFloor, self.passenger_num))
-        print("DEACTIVE")
+        print("{} people exits".format('xxx'))
         self.fromFloor = self.toFloor
-        self.active = False
+        if self.waiting_list[self.fromFloor] != []:
+            self.waiting_num -= len(self.waiting_list[self.fromFloor])
+            self.passenger_num += len(self.waiting_list[self.fromFloor])
+            print("{} people enter elevator".format(len(self.waiting_list[self.fromFloor])))   
+            self.toTransport += self.waiting_list[self.fromFloor]
+            self.waiting_list[self.fromFloor] = []
+            print('toTransport: ', self.toTransport)
+        if len(self.toTransport) == 0:
+            print("DEACTIVE")
+            self.active = False
 
     def set_new_passenger(self, atFloor, toFloor):
         if atFloor == toFloor:
             return
-        self.waiting_list[atFloor].append((atFloor, toFloor))
+        self.waiting_list[atFloor].append(toFloor)
         self.waiting_num += 1
+        print('waiting_list:', self.waiting_list)
 
     def get_passenger_from_waiting(self):
         if self.active == False:
@@ -42,15 +58,15 @@ class Elevator(threading.Thread):
             # print(sorted(self.waiting_list.items(), key= lambda t: len(t[1])))
             # print(sorted(self.waiting_list.items(), key= lambda t: len(t[1])).pop()[0])
             floor = sorted(self.waiting_list.items(), key= lambda t: len(t[1])).pop()[0]
-            atFloor, toFloor = self.waiting_list[floor].pop()
-            self.waiting_num -= 1
-            self.take_passenger(atFloor)
-            self.transport(toFloor)      
+            # need to implement a waiting prioirtiy to choose which floor the elevator should go to
+            # now we take the floor the most people waiting
+            self.take_passenger(floor)
 
     def run(self):
         while True:
-            time.sleep(0.1)
-            if len(self.toPass) == 0 and self.waiting_num != 0:
+            if len(self.toTransport) != 0:
+                self.transport()
+            elif len(self.toTransport) == 0 and self.waiting_num != 0:
                 self.get_passenger_from_waiting()
 
 
@@ -66,4 +82,4 @@ if __name__ == "__main__":
         print('passenger:', atFloor, toFloor)
         newPassenger = threading.Thread(target=elevator.set_new_passenger, args=(atFloor, toFloor, ))
         newPassenger.start()
-        time.sleep(3)
+        time.sleep(0.05)
